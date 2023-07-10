3bfafd689b188fafc6a8e28c15d4735d00a9d3dd
kongchengz <ykr2323464705@gmail.com>
2023-07-10 10:53:39 +0800

    新增Multiscale Deformable Attention组件

--- src/utils/misc.py
+++ src/utils/misc.py
@@ -182,3 +182,10 @@ def nested_dict_to_device(dictionary, device):
         return output
 
     return dictionary.to(device)
+
+def inverse_sigmoid(x, eps=1e-5):
+    x = x.clamp(min=0, max=1)
+    x1 = x.clamp(min=eps)
+    x2 = (1 - x).clamp(min=eps)
+
+    return torch.log(x1 / x2)
