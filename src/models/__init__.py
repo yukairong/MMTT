af20a453d39c91737f376975845b5b41f91c62c5
fancy <47590538+yuguopin@users.noreply.github.com>
2023-07-10 22:29:50 +0800

    模型建立时添加criterion

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
 import torch
-
+from models.detr import SetCriterion
 from models.backbone import build_backbone
 from models.matcher import build_matcher
 from models.transformer import build_transformer
@@ -18,12 +18,9 @@ def build_model(args):
         raise NotImplementedError
 
     device = torch.device(args.device)
-
     # 构建backbone以及 matcher
     backbone = build_backbone(args)
     matcher = build_matcher(args)
-
-    transformer = build_transformer(args)
 
     # version 1
     mmtt_v1_kwargs = {
@@ -40,7 +37,40 @@ def build_model(args):
         "backprop_prev_frame": args.track_backprop_prev_frame,
     }
 
+    transformer = build_transformer(args)
     if args.deformable:
         transformer = build_deformable_transformer(args)
         print("Deformable Transformer Model: ")
         print(transformer)
+
+    # 损失函数的构建
+    weight_dict = {'loss_ce': args.cls_loss_coef,
+                   'loss_bbox': args.bbox_loss_coef,
+                   'loss_giou': args.giou_loss_coef, }
+
+    # 是否使用辅助训练
+    if args.aux_loss:
+        aux_weight_dict = {}
+        for i in range(args.dec_layers - 1):
+            aux_weight_dict.update({k + f'_{i}': v for k, v in weight_dict.items()})
+
+        if args.two_stage:
+            aux_weight_dict.update({k + f'_enc': v for k, v in weight_dict.items()})
+        weight_dict.update(aux_weight_dict)
+
+    losses = ['labels', 'boxes', 'cardinality']
+
+    criterion = SetCriterion(
+        num_classes,
+        matcher=matcher,
+        weight_dict=weight_dict,
+        eos_coef=args.eos_coef,
+        losses=losses,
+        focal_loss=args.focal_loss,
+        focal_alpha=args.focal_alpha,
+        focal_gamma=args.focal_gamma,
+        tracking=args.tracking,
+        track_query_false_positive_eos_weight=args.track_query_false_positive_eos_weight, )
+    criterion.to(device)
+
+
