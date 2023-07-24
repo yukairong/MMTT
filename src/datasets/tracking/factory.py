648bf209961514293a5ce049da46ff8c40cfc258
fancy <47590538+yuguopin@users.noreply.github.com>
2023-07-24 14:23:14 +0800

    wildtrack测试数据集的类创建

--- src/datasets/tracking/factory.py
+++ src/datasets/tracking/factory.py
@@ -2,15 +2,23 @@ from typing import Union
 
 from torch.utils.data import ConcatDataset
 
-DATASETS = {}
+from src.datasets.tracking.demo_sequence import DemoSequence
+from src.datasets.tracking.wildTrack_sequence import WildTrackSequence
+
+# 将所需可能用到的测试数据集都放在这个字典里
+DATASETS = {
+    'DEMO': (lambda kwargs: [DemoSequence(**kwargs), ]),
+    'WildTrack': (lambda kwargs: [WildTrackSequence(**kwargs), ])
+}
+
 
 class TrackDatasetFactory:
-    """A central class to manage the individual dataset loaders
-
-    This class contains the datasets. Once initialized the individual parts (e.g. sequences)
-    can be accessed
+    """
+    A central class to manage the individual dataset loaders
+    This class contains the datasets. Once initialized the individual parts (e.g. sequences) can be accessed
     """
 
+    # datasets传进来的参数是 'WildTrack
     def __init__(self, datasets: Union[str, list], **kwargs) -> None:
         """
         Initialize the corresponding dataloader
@@ -23,3 +31,14 @@ class TrackDatasetFactory:
         self._data = None
         for dataset in datasets:
             assert dataset in DATASETS, f"[!] Dataset not found: {dataset}"
+
+            if self._data is None:
+                self._data = DATASETS[dataset](kwargs)
+            else:
+                self._data = ConcatDataset([self._data, DATASETS[dataset](kwargs)])
+
+    def __len__(self) -> int:
+        return len(self._data)
+
+    def __getitem__(self, idx: int):
+        return self._data[idx]
