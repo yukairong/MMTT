211bf1583ffa9f5e9137a6c51b911d53dddde252
fancy <47590538+yuguopin@users.noreply.github.com>
2023-09-06 22:39:43 +0800

    修改优化器为Adam

--- gnn_trainer.py
+++ gnn_trainer.py
@@ -117,8 +117,7 @@ class GnnTrainer:
              {"params": self.edge_feature_encoder.parameters()},
              {"params": self.mpn.parameters()},
              {"params": self.predictor.parameters()}],
-            lr=0.15,
-            momentum=0.9
+            lr=0.15
         )
         lr_warmup_list = np.linspace(0, 0.01, 5 + 1, endpoint=False)
         lr_warmup_list = lr_warmup_list[1:]
